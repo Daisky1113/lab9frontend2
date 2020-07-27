77091e6810a8b5924d2fbc29bbf84068b9bee529
Daisky <nakamura.daisky@gmail.com>
2020-07-28 03:51:38 +0900

    buttonsコンポーネント

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,123 +1,20 @@
 <template>
   <v-container>
     <v-row justify="center" align="center">
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6">
+      <v-col cols="6">
         <v-card>
           <v-card-title>Hello</v-card-title>
           <v-card-subtitle>world</v-card-subtitle>
           <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-card-text>
           <v-card-actions>
-            <v-btn text>foo</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
-        <v-card>
-          <v-card-title>Hello</v-card-title>
-          <v-card-subtitle>world</v-card-subtitle>
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-card-text>
-          <v-card-actions>
-            <v-btn text>foo</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
-        <v-card>
-          <v-card-title>Hello</v-card-title>
-          <v-card-subtitle>world</v-card-subtitle>
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-card-text>
-          <v-card-actions>
-            <v-btn text>foo</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
-        <v-card>
-          <v-card-title>Hello</v-card-title>
-          <v-card-subtitle>world</v-card-subtitle>
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-card-text>
-          <v-card-actions>
-            <v-btn text>foo</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
-        <v-card>
-          <v-card-title>Hello</v-card-title>
-          <v-card-subtitle>world</v-card-subtitle>
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-card-text>
-          <v-card-actions>
-            <v-btn text>foo</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
-        <v-card>
-          <v-card-title>Hello</v-card-title>
-          <v-card-subtitle>world</v-card-subtitle>
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-card-text>
-          <v-card-actions>
-            <v-btn text>foo</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
-        <v-card>
-          <v-card-title>Hello</v-card-title>
-          <v-card-subtitle>world</v-card-subtitle>
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-card-text>
-          <v-card-actions>
-            <v-btn text>foo</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
-        <v-card>
-          <v-card-title>Hello</v-card-title>
-          <v-card-subtitle>world</v-card-subtitle>
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-card-text>
-          <v-card-actions>
-            <v-btn text>foo</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
-        <v-card>
-          <v-card-title>Hello</v-card-title>
-          <v-card-subtitle>world</v-card-subtitle>
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-card-text>
-          <v-card-actions>
-            <v-btn text>foo</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
-        <v-card>
-          <v-card-title>Hello</v-card-title>
-          <v-card-subtitle>world</v-card-subtitle>
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-card-text>
-          <v-card-actions>
-            <v-btn text>foo</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
-        <v-card>
-          <v-card-title>Hello</v-card-title>
-          <v-card-subtitle>world</v-card-subtitle>
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-card-text>
-          <v-card-actions>
-            <v-btn text>foo</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
-        <v-card>
-          <v-card-title>Hello</v-card-title>
-          <v-card-subtitle>world</v-card-subtitle>
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-card-text>
-          <v-card-actions>
-            <v-btn text>foo</v-btn>
+            <v-btn>lorem</v-btn>
+            <v-btn outlined>ipsum</v-btn>
+            <v-btn rounded>dolor</v-btn>
+            <v-btn tile>sit</v-btn>
+            <v-btn text>amet</v-btn>
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
