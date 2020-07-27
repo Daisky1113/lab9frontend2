a503189b0cbcc93f43849724421898789e744699
Daisky <nakamura.daisky@gmail.com>
2020-07-28 05:26:26 +0900

    importしたLoginFormコンポーネントをログインページ内で使う

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,20 @@
 <template>
   <v-container>
     <v-row justify="center" align="center">
-      <v-col cols="4"></v-col>
+      <v-col cols="4">
+        <LoginForm />
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
 import LoginForm from "../components/Loginform";
+
+export default {
+  name: "Login",
+  components: {
+    LoginForm,
+  },
+};
 </script>
