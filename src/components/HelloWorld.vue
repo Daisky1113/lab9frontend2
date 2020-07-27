98c565cad4934c9a9de67969e5add134691d7eea
Daisky <nakamura.daisky@gmail.com>
2020-07-28 03:32:24 +0900

    v-cardコンポーネント
    v-cardコンポーネントは何かを囲みたい時に使う汎用コンポーネント。

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,42 @@
 <template>
   <v-container>
     <v-row justify="center" align="center">
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6">foo1</v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo2</v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo3</v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo4</v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo5</v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo6</v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo7</v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo8</v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo9</v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo10</v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo11</v-col>
-      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo12</v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6">
+        <v-card>foo1</v-card>
+      </v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
+        <v-card>foo2</v-card>
+      </v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
+        <v-card>foo3</v-card>
+      </v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
+        <v-card>foo4</v-card>
+      </v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
+        <v-card>foo5</v-card>
+      </v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
+        <v-card>foo6</v-card>
+      </v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
+        <v-card>foo7</v-card>
+      </v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
+        <v-card>foo8</v-card>
+      </v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
+        <v-card>foo9</v-card>
+      </v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
+        <v-card>foo10</v-card>
+      </v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
+        <v-card>foo11</v-card>
+      </v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">
+        <v-card>foo12</v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
