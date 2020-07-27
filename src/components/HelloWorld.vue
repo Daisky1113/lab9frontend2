22cde1ded835bd236233afaaa515344f7c96ebf5
Daisky <nakamura.daisky@gmail.com>
2020-07-28 04:16:59 +0900

    textfieldsコンポーネント

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,18 +3,19 @@
     <v-row justify="center" align="center">
       <v-col cols="6">
         <v-card>
-          <v-card-title>Hello</v-card-title>
-          <v-card-subtitle>world</v-card-subtitle>
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-card-text>
+          <v-card-title>ログイン</v-card-title>
+          <v-card-text>
+            <v-text-field outlined label="email" prepend-inner-icon="mdi-email-outline"></v-text-field>
+            <v-text-field
+              outlined
+              type="password"
+              label="password"
+              prepend-inner-icon="mdi-lock-outline"
+            ></v-text-field>
+          </v-card-text>
           <v-card-actions>
-            <v-btn>lorem</v-btn>
-            <v-btn outlined>ipsum</v-btn>
-            <v-btn rounded>dolor</v-btn>
-            <v-btn tile>sit</v-btn>
-            <v-btn text>amet</v-btn>
-            <v-btn icon>
-              <v-icon>mdi-heart</v-icon>
-            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn outlined>ログインする</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
