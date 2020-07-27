b48792abb284cf3fa41f29f47fbc38311175f5a2
Daisky <nakamura.daisky@gmail.com>
2020-07-28 05:07:50 +0900

    ログインフォームコンポーネントのtemplate部分を作る
    vuetifyから適当にコピペしてくる。この時点で見た目は特に気にしなくて良い。

---
+++ src/components/Loginform.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card>
+    <v-card-title>ログイン</v-card-title>
+    <v-card-text>
+      <v-text-field outlined label="email" prepend-inner-icon="mdi-email-outline"></v-text-field>
+      <v-text-field outlined type="password" label="password" prepend-inner-icon="mdi-lock-outline"></v-text-field>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn text>ログインする</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
