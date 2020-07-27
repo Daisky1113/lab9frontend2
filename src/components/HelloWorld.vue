c561378633d9389f3ec7df6111ec7e4059dbaa3e
Daisky <nakamura.daisky@gmail.com>
2020-07-28 03:15:24 +0900

    vuetifyのグリッドシステム
    基本的にはbootstrapと同じ仕組みだがxsは動かない（ライブラリのバグ）なのでxsはcolsで指定する

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,20 @@
 <template>
-  <v-container></v-container>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6">foo1</v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo2</v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo3</v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo4</v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo5</v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo6</v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo7</v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo8</v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo9</v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo10</v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo11</v-col>
+      <v-col cols="12" xl="2" lg="3" md="4" sm="6" xs="12">foo12</v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
