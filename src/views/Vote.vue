fe30567b5ba79e3182face1b18e945b9169671e1
Daisky <nakamura.daisky@gmail.com>
2020-07-28 15:43:03 +0900

    VoteDialog componentをVote.vue内でimportする

--- src/views/Vote.vue
+++ src/views/Vote.vue
@@ -2,6 +2,7 @@
   <v-container>
     <v-row justify="center" align="center">
       <v-col cols="12">
+        <VoteDialog />
         <VoteTab />
       </v-col>
     </v-row>
@@ -9,11 +10,12 @@
 </template>
 <script>
 import VoteTab from "../components/VoteTab";
-
+import VoteDialog from "../components/VoteDialog";
 export default {
   name: "Vote",
   components: {
     VoteTab,
+    VoteDialog,
   },
 };
 </script>
