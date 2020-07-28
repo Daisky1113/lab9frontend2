cc03899423fd766141832b5ba0d12361fa0759dd
Daisky <nakamura.daisky@gmail.com>
2020-07-29 03:40:27 +0900

    モックデータにproductInfoコンポーネントに渡す用のデータを定義

--- src/components/VoteTab.vue
+++ src/components/VoteTab.vue
@@ -26,6 +26,14 @@ export default {
       return new Array(36).fill(null).map((el, index) => ({
         id: index + 1,
         name: "member-" + (index + 1),
+        productName: "product-" + (index + 1),
+        tecTopix: "Lorem,ipsum,dolor,sit,amet,consectetur,adipiscing",
+        tecDetail:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        serviceDetail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          .split("")
+          .reverse()
+          .join(""),
       }));
     },
   },
