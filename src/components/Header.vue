511d2bfc60fcd0aea502b84d2c78a72d66dcfa57
Daisky <nakamura.daisky@gmail.com>
2020-07-29 06:48:10 +0900

    Headerコンポーネントにページ遷移用のボタンを作成

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,26 +8,20 @@
       <v-spacer></v-spacer>
 
       <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
+        <v-icon>mdi-vote-outline</v-icon>
       </v-btn>
 
       <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
+        <v-icon>mdi-clipboard-text-outline</v-icon>
       </v-btn>
 
-      <v-menu left bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon v-bind="attrs" v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
+      <v-btn icon>
+        <v-icon>mdi-clipboard-edit-outline</v-icon>
+      </v-btn>
 
-        <v-list>
-          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+      <v-btn icon>
+        <v-icon>mdi-logout-variant</v-icon>
+      </v-btn>
     </v-app-bar>
   </div>
 </template>
