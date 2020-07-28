920b51a9b3046897cff3dae6000eebea386006aa
Daisky <nakamura.daisky@gmail.com>
2020-07-29 01:59:38 +0900

    プロダクト情報ページでproductDetailコンポーネントを読み込む

--- src/views/ProductInfo.vue
+++ src/views/ProductInfo.vue
@@ -1,7 +1,9 @@
 <template>
   <v-container fluid>
-    <v-row justify="center" align="center">
-      <v-col cols="8"></v-col>
+    <v-row justify="center" align>
+      <v-col cols="8">
+        <ProductDetail />
+      </v-col>
       <v-col cols="4">
         <ProductForm />
       </v-col>
@@ -10,10 +12,12 @@
 </template>
 <script>
 import ProductForm from "../components/ProductForm";
+import ProductDetail from "../components/ProductDetails";
 export default {
   name: "ProductInfo",
   components: {
     ProductForm,
+    ProductDetail,
   },
 };
 </script>
