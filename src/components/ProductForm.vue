02a1c60aaab12f816a599c819ef2889e2255c927
Daisky <nakamura.daisky@gmail.com>
2020-07-29 00:48:22 +0900

    プロダクト情報登録フォームを作る
    data methodsも定義

---
+++ src/components/ProductForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card flat>
+    <v-card-text>
+      <v-text-field v-model="inputData.productName" outlined label="プロダクト名"></v-text-field>
+      <v-textarea v-model="inputData.tecTopix" outlined label="使用技術"></v-textarea>
+      <v-textarea v-model="inputData.tecDetail" outlined label="技術的に工夫したところ"></v-textarea>
+      <v-textarea v-model="inputData.serviceDetail" outlined label="サービス的に工夫したところ"></v-textarea>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn @click="regist" text>登録する</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  name: "ProductInfo",
+  data: () => ({
+    inputData: {
+      productName: "",
+      tecTopix: "",
+      tecDetail: "",
+      serviceDetail: "",
+    },
+  }),
+  methods: {
+    regist() {
+      console.log(this.inputData);
+    },
+  },
+};
+</script>
