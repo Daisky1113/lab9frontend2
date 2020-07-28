4be0c8f1d431052250a63b008eaa6103ab0d7b6b
Daisky <nakamura.daisky@gmail.com>
2020-07-28 17:46:03 +0900

    一覧用ページでVoteListコンポーネントをimportする

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,9 +1,18 @@
 <template>
   <v-container>
     <v-row justify="center" align="center">
-      <v-col cols="12"></v-col>
+      <v-col cols="12">
+        <VoteList />
+      </v-col>
     </v-row>
   </v-container>
 </template>
 <script>
+import VoteList from "../components/VoteList";
+export default {
+  name: "List",
+  components: {
+    VoteList,
+  },
+};
 </script>
