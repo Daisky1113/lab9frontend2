e1aedf103ec471a4b950e0ced142a1cd25453837
Daisky <nakamura.daisky@gmail.com>
2020-07-29 02:37:27 +0900

    一覧ページでVoteDialogコンポーネントを読み込む

--- src/views/List.vue
+++ src/views/List.vue
@@ -2,6 +2,7 @@
   <v-container>
     <v-row justify="center" align="center">
       <v-col cols="12">
+        <VoteDialog />
         <VoteList />
       </v-col>
     </v-row>
@@ -9,10 +10,12 @@
 </template>
 <script>
 import VoteList from "../components/VoteList";
+import VoteDialog from "../components/VoteDialog";
 export default {
   name: "List",
   components: {
     VoteList,
+    VoteDialog,
   },
 };
 </script>
