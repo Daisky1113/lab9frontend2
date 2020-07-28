5cbe2479c092a35b37ef2cc0e7c3ad5e479c8d3f
Daisky <nakamura.daisky@gmail.com>
2020-07-29 00:48:55 +0900

    プロダクト情報ページを作る。登録フォームもimportする

---
+++ src/views/ProductInfo.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container fluid>
+    <v-row justify="center" align="center">
+      <v-col cols="8"></v-col>
+      <v-col cols="4">
+        <ProductForm />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+import ProductForm from "../components/ProductForm";
+export default {
+  name: "ProductInfo",
+  components: {
+    ProductForm,
+  },
+};
+</script>
