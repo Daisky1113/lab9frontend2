56c3a6a9e25a9ec706d8a7a68e343099b4fe5660
Daisky <nakamura.daisky@gmail.com>
2020-08-06 11:39:50 +0900

    firebaseを使う準び

--- src/components/Loginform.vue
+++ src/components/Loginform.vue
@@ -37,6 +37,7 @@
 <script>
 export default {
   name: "LoginForm",
+
   data: () => ({
     isSignup: false,
     inputData: {
--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
+import firebaseConfig from './firebaseconfig'
 
 Vue.config.productionTip = false
 
+firebase.initializeApp(firebaseConfig);
 new Vue({
   router,
   store,
