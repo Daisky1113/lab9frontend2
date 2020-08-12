a8fa43aa0f077ba0a5d974ec7a5b8fa91564d20b
Daisky <nakamura.daisky@gmail.com>
2020-08-13 04:30:36 +0900

    各入力エリアの@inputにmutationを アタッチする

--- src/components/ProductForm.vue
+++ src/components/ProductForm.vue
@@ -1,10 +1,25 @@
 <template>
   <v-card flat>
     <v-card-text>
-      <v-text-field :value="productData.productName" outlined label="プロダクト名"></v-text-field>
-      <v-textarea :value="productData.tecTopix" outlined label="使用技術"></v-textarea>
-      <v-textarea :value="productData.tecDetail" outlined label="技術的に工夫したところ"></v-textarea>
-      <v-textarea :value="productData.serviceDetail" outlined label="サービス的に工夫したところ"></v-textarea>
+      <v-text-field
+        @input="updateProductName"
+        :value="productData.productName"
+        outlined
+        label="プロダクト名"
+      ></v-text-field>
+      <v-textarea @input="updatetecTopix" :value="productData.tecTopix" outlined label="使用技術"></v-textarea>
+      <v-textarea
+        @input="updatetecDetail"
+        :value="productData.tecDetail"
+        outlined
+        label="技術的に工夫したところ"
+      ></v-textarea>
+      <v-textarea
+        @input="updateServiceDetail"
+        :value="productData.serviceDetail"
+        outlined
+        label="サービス的に工夫したところ"
+      ></v-textarea>
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
